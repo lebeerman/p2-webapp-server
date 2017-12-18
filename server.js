81cad0da2f9298167b9754ec09849a7354d1af10
lebeerman <omnidbeerman@gmail.com>
2017-12-18 14:50:27 -0700

    skeleton heroku server running

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const data = require('./data/data');
+const data = require('data/data');
 const app = express();
-const path = require('path');
+// const path = require('path');
 const cors = require('cors');
 
 app.use(cors);
 
 app.get('/', (req, res) => res.json(data));
 
-app.listen(process.env.PORT || 3000, () => console.log('yay, running on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('yay, running on port!'))
