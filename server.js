c853716b2dc682971a55fae44d2c517b4e4940b9
lebeerman <omnidbeerman@gmail.com>
2017-12-18 13:44:58 -0700

    port added

--- server.js
+++ server.js
@@ -7,4 +7,4 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.json(data));
 
-app.listen(3000, () => console.log('yay, running on port 3000!'))
+app.listen(PORT, () => console.log('yay, running on port 3000!'))
