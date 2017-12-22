280097b19e75c8e11ee371336416bcfd2a7f616b
lebeerman <omnidbeerman@gmail.com>
2017-12-21 22:58:00 -0700

    building API. Connecting to Lealet. Connecting firebase app to server routes.

--- data/data.js
+++ data/data.js
@@ -1,11 +1,14 @@
-module.exports = {
-  data: [{
-      "id": 1,
-      "firstAttribute": "firstValue",
-      "secondAttribute": "secondValue"
+const data = [{
+      id: 1,
+      firstAttribute: "firstValue",
+      secondAttribute: "secondValue"
   },{
-      "id": 2,
-      "firstAttribute": "firstValue",
-      "secondAttribute": "secondValue"
-  }]
+      id: 2,
+      firstAttribute: "firstValue",
+      secondAttribute: "secondValue"
+  }];
+
+
+module.exports = {
+  data
 }
--- server.js
+++ server.js
@@ -7,4 +7,11 @@ app.use(cors());
 
 app.get('/', (req, res) => res.json(data));
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port!'))
+app.listen(process.env.PORT || 3000, () => console.log(`app listening on port ${process.env.PORT || 3000}!`))
+
+// 
+//
+// fetch(url)
+//   .then(response => response.json())
+//   .then()
+// .catch(console.error)
