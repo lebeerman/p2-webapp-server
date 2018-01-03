d9ee6982befe47f6254518daef0377fc7880e258
lebeerman <omnidbeerman@gmail.com>
2018-01-03 16:13:10 -0700

    server working, no post or unit tests.

--- data/data.js
+++ data/data.js
@@ -1,14 +1,40 @@
-const data = [{
-      id: 1,
-      firstAttribute: "firstValue",
-      secondAttribute: "secondValue"
+const toolTip = [{
+    id: 1,
+    locationTitle: "Galvanize Denver - Platte",
+    locationNote: "This is basically where g70 lives."
   },{
-      id: 2,
-      firstAttribute: "firstValue",
-      secondAttribute: "secondValue"
-  }];
-
-
+    id: 2,
+    locationTitle: "My Brothers Bar",
+    locationNote: "Affordable lunch spot. $7 Burgers!"
+  },{
+    id: 3,
+    locationTitle: "Free parking!",
+    locationNote: "Just watch out for street cleaning!"
+  },{
+    id: 4,
+    locationTitle: "Apex Park",
+    locationNote: "A fantastic running spot in the AM or PM, super crowded on the weekends... damn mountain bikers."
+  }
+];
+const markerInfo = [{
+    id: 1,
+    latLong: [39.75779, -105.00715],
+    markerType: "marker"
+  },{
+    id: 2,
+    latLong: [39.75623, -105.00923],
+    markerType: "marker"
+  },{
+    id: 3,
+    latLong: [39.75893, -105.01311],
+    markerType: "circle"
+  },{
+    id: 4,
+    latLong: [39.72118, -105.21658],
+    markerType: "marker"
+  }
+];
 module.exports = {
-  data
+  toolTip,
+  markerInfo
 }
--- server.js
+++ server.js
@@ -9,8 +9,6 @@ app.get('/', (req, res) => res.json(data));
 
 app.listen(process.env.PORT || 3000, () => console.log(`app listening on port ${process.env.PORT || 3000}!`))
 
-// 
-//
 // fetch(url)
 //   .then(response => response.json())
 //   .then()
